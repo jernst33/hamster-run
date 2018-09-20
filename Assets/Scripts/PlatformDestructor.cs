032902c70701d091c38c261c223f8ce545104c75
sjredwing33 <jernst4601@gmail.com>
2018-09-20 08:40:34 -0400

    Added an animated sprite to progress through level

---
+++ Assets/Scripts/PlatformDestructor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestructor : MonoBehaviour
+{
+
+    public GameObject destructionPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        destructionPoint = GameObject.Find("PlatformDestructionPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < destructionPoint.transform.position.x)
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject platform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    public ObjectPooler objectPool;
+
+    public GameObject[] platforms;
+    private int platformSelector;
+
+    private float[] platformWidths;
+
+	// Use this for initialization
+	void Start () {
+
+        //platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+        platformWidths = new float[platforms.Length];
+
+        for(int i = 0; i < platforms.Length; i++)
+        {
+            platformWidths[i] = platforms[i].GetComponent<BoxCollider2D>().size.x;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+            // Add platform width and distance between to X coordinate
+            platformSelector = Random.Range(0, platforms.Length);
+
+            transform.position = new Vector3 (transform.position.x + platformWidths[platformSelector]  /*distanceBetween*/, transform.position.y, transform.position.z);
+
+            /* //Instantiate(platform, transform.position, transform.rotation);
+             GameObject newPlatform = objectPool.GetPooledObject();
+
+             newPlatform.transform.position = transform.position;
+             newPlatform.transform.rotation = transform.rotation;
+             newPlatform.SetActive(true); */
+
+            Instantiate(platforms[platformSelector], transform.position, transform.rotation);
+        }
+	}
+}
